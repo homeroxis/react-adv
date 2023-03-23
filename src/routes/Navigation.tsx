a55ba562da87d183ac34f9ec0c3232782e86992a
homeroxis <juliandres@gmail.com>
2023-03-23 16:52:11 -0300

    Primer commit

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,45 @@
+import { Suspense } from 'react';
+import { BrowserRouter, Route, NavLink, Routes, Navigate } from 'react-router-dom';
+
+import logo from '../logo.svg';
+import { routes } from './routes';
+
+export const Navigation = () => {
+  return (
+    <Suspense fallback={ <span>Loading...</span> }>
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+              <img src={ logo } alt="React Logo" />
+            <ul>
+              { routes.map( ({ name, to,  }) => (
+                <li key={ name }>
+                  <NavLink
+                    to={to}
+                    className={ ({ isActive }) => isActive ? 'nav-active' : '' }
+                  >
+                    { name }
+                  </NavLink>
+                </li>
+                ))
+              }
+            </ul>
+          </nav>
+
+          <Routes>
+            {
+              routes.map(({name, path, Component}) => (
+                <Route
+                  key={name}
+                  path={path}
+                  element={ <Component /> }
+                />
+              ))
+            }
+            <Route path="/*" element={ <Navigate to={ routes[0].to } replace /> } />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
+  );
+}
